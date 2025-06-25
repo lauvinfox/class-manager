3e95a7f692c2e908fb8af5573d2ba14790f4de6b
lauvinfox <miftahilmanakun@gmail.com>
2025-06-25 19:32:07 +0700

    refactor: class header

---
+++ frontend/src/components/ClassHeader.tsx
@@ -0,0 +1,40 @@
+import { ClassHeaderProps } from "../types/types";
+
+export const ClassHeader = ({
+  title,
+  activeTab,
+  handleTab,
+}: ClassHeaderProps) => {
+  const tabs = [
+    "Overview",
+    "Statistics",
+    "Instructors",
+    "Students",
+    "Assignments",
+  ];
+  return (
+    <div className="relative top-0 z-40 flex flex-col gap-1 justify-between bg-primary border-b border-slate-200 px-6 dark:bg-gray-900 dark:border-gray-800 min-w-0">
+      <span className="flex py-4 text-lg font-semibold text-font-primary dark:text-white select-none">
+        {title}
+      </span>
+      <div className="flex border-b gap-5 border-slate-200 dark:border-gray-700 bg-primary dark:bg-gray-900">
+        {tabs.map((tab) => (
+          <div
+            key={tab}
+            className={`py-2 pb-4 cursor-pointer text-sm font-medium transition-colors relative ${
+              activeTab === tab
+                ? "text-indigo-700 dark:text-white"
+                : "text-slate-500 dark:text-slate-300 hover:text-indigo-700 dark:hover:text-white"
+            }`}
+            onClick={() => handleTab(tab)}
+          >
+            {tab}
+            {activeTab === tab && (
+              <div className="absolute left-0 right-0 -bottom-0.5 h-0.5 bg-indigo-600 rounded-t" />
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
--- frontend/src/pages/Class.tsx
+++ frontend/src/pages/Class.tsx
@@ -12,8 +12,9 @@ import { useTheme } from "../contexts/ThemeContext";
 
 import { addStudentsToClass, getClassByClassId, getUserInfo } from "../lib/api";
 import { useMutation } from "@tanstack/react-query";
-import { ClassHeaderProps, ClassInfo } from "../types/types";
+import { ClassInfo } from "../types/types";
 import InstructorsTab from "../components/InstructorsTab";
+import { ClassHeader } from "../components/ClassHeader";
 
 const ClassPage = () => {
   const { darkMode } = useTheme();
@@ -358,35 +359,6 @@ const ClassPage = () => {
         </div>
       </div>
     </AuthProvider>
-  );
-};
-
-const ClassHeader = ({ title, activeTab, handleTab }: ClassHeaderProps) => {
-  const tabs = ["Overview", "Statistics", "Instructors", "Students"];
-  return (
-    <div className="relative top-0 z-40 flex flex-col gap-1 justify-between bg-primary border-b border-slate-200 px-6 dark:bg-gray-900 dark:border-gray-800 min-w-0">
-      <span className="flex py-4 text-lg font-semibold text-font-primary dark:text-white select-none">
-        {title}
-      </span>
-      <div className="flex border-b gap-5 border-slate-200 dark:border-gray-700 bg-primary dark:bg-gray-900">
-        {tabs.map((tab) => (
-          <div
-            key={tab}
-            className={`py-2 pb-4 cursor-pointer text-sm font-medium transition-colors relative ${
-              activeTab === tab
-                ? "text-indigo-700 dark:text-white"
-                : "text-slate-500 dark:text-slate-300 hover:text-indigo-700 dark:hover:text-white"
-            }`}
-            onClick={() => handleTab(tab)}
-          >
-            {tab}
-            {activeTab === tab && (
-              <div className="absolute left-0 right-0 -bottom-0.5 h-0.5 bg-indigo-600 rounded-t" />
-            )}
-          </div>
-        ))}
-      </div>
-    </div>
   );
 };
 
